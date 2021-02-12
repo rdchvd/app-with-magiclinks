2995d489611c676a527518d03bdec05bf7ceada4
rdchvd <rodicevadarina@gmail.com>
2021-02-12 12:42:53 +0200

    remove tests

--- magic_project/app/tests.py
+++ magic_project/app/tests.py
@@ -1,8 +1,1 @@
 from django.test import TestCase
-from .models import User
-
-class UserTestCase(TestCase):
-    def setUp(self):
-        User.objects.create(email='kek@email.com')
-        #u = User.objects.get(email='kek@email.com')
-        #print(u)
